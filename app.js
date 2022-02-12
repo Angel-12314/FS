0e243e9b3916e6b8ece266b93a2ab143d41d5d8b
Angel-12314 <angelpriyankag.14@gmail.com>
2022-02-12 12:00:16 +0530

    null vs undefined

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-//string
-console.log(typeof "Hello World")
-console.log(typeof "d")
+var age = null 
 
-//Numbers
-console.log(typeof 1)
-console.log(typeof 1.2)
-console.log(typeof 112655332326901234321790979874432322190)
-console.log(typeof -1)
-console.log(typeof -1.2)
-
-//boolean
-console.log(typeof true)
-console.log(typeof false)
+// console.log(Name)
+console.log(typeof age)
