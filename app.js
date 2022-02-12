2dc194698d4dc42797c9534f16247a22cc1fe3de
Angel-12314 <angelpriyankag.14@gmail.com>
2022-02-12 12:15:52 +0530

    operators

--- app.js
+++ app.js
@@ -1,4 +1,54 @@
-var age = null 
+// operators
+var a=2
+var b=10
 
-// console.log(Name)
-console.log(typeof age)
+//Arithmetic operators
+
+console.log('a + b =', a+b)
+console.log('a - b =', a-b)
+console.log('a / b =', a/b)
+console.log('a * b =', a*b)
+console.log('a ** b =', a**b)
+console.log('a % b =', a%b)
+
+// Assignment Operators
+console.log('b', b)
+b +=10
+console.log('b', b)
+b++
+console.log('b', b)
+b--
+console.log('b', b)
+
+var c=10
+
+console.warn(c++)
+console.log(c)
+//console.warn(++c)
+
+//Comparision Operators
+
+var d = 10
+var e = 2
+var f ='10'
+
+console.log(d > e)
+console.log(d < e)
+console.log(d >= e)
+console.log(d <= e)
+console.log(d == e)
+
+console.warn(d == f)
+console.warn(d === f)
+
+//Logical operators
+
+console.log('true && true',true && true)
+console.log('true && false',true && false)
+console.log('false && true',false && true)
+console.log('false && false',false && false)
+
+console.log('true || true ',true || true)
+console.log('true || false',true || false)
+console.log('false || true',false || true)
+console.log('false || false',false || false)
