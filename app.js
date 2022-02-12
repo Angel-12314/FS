af54b2662434e5196f07f45b5d8a8fdeffa7ebb3
Angel-12314 <angelpriyankag.14@gmail.com>
2022-02-12 11:56:25 +0530

    primitive data-types

---
+++ app.js
@@ -0,0 +1,14 @@
+//string
+console.log(typeof "Hello World")
+console.log(typeof "d")
+
+//Numbers
+console.log(typeof 1)
+console.log(typeof 1.2)
+console.log(typeof 112655332326901234321790979874432322190)
+console.log(typeof -1)
+console.log(typeof -1.2)
+
+//boolean
+console.log(typeof true)
+console.log(typeof false)
