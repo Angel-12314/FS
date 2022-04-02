137938d989cffa6175d4199f48216539c8d32209
Angel-12314 <angelpriyankag.14@gmail.com>
2022-04-02 11:57:24 +0530

    calculator

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
-var body = document.querySelector('body')
-var button = document.querySelector('button')
+const a = document.querySelectorAll('input')[0]
+const b = document.querySelectorAll('input')[1]
+const add = document.querySelector('#add')
+const multiply = document.querySelector('#multiply')
+const resultBox = document.querySelector('.result')
 
+const sum = () => {
 
-const callbackfunc = (event) => {
-    body.classList.toggle('dark')
+}
+
+const product = () => {
+    const result = parseInt(a.value) * parseInt(b.value)
+    resultBox.innerHTML = result 
 }
 
 
-button.addEventListener('click', callbackfunc)
+const calculate = (event,operation) => {
+    //if (operation == 'add') {
+    //    console.log('adding')
+    //} else if (operation == 'multiply') {
+    //   console.log('multiply')
+    //}
+    switch (operation) {
+        case "add":
+            resultBox.innerHTML = parseInt(a.value) + parseInt(b.value)
+            break
+        case 'multiply':
+            resultBox.innerHTML = parseInt(a.value) * parseInt(b.value)
+            break
+        default:
+            resultBox.innerHTML = "Not a valid operation"
+
+    }
+}
+
+
+add.addEventListener('click',(event) => {
+    calculate(event,'add')
+})
+
+multiply.addEventListener('click',(event) => {
+    calculate(event,'multiply')
+})
