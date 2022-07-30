df666e1ec53ae4691daf09736f0c7165e995d025
Angel-12314 <angelpriyankag.14@gmail.com>
2022-07-30 10:02:00 +0530

    promises

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-var arr = [1, 2, 3, 4, 5, 6, 7, 8]
-var new_arr = arr.filter((element, index) => {
-    if(element < 100) return true
+let response = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(data => {
+    console.log(data)
 })
-
-
-
-// for (var i=0;i<arr.length;i++) {
-//    if (arr[i] %2 ===0) new_arr.push(arr[i])
-//}
-
-
-console.log(new_arr)
