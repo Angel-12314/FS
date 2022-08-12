80117568c37a0d66b2344169c49b3f68cc606152
Angel-12314 <angelpriyankag.14@gmail.com>
2022-08-12 16:39:09 +0530

    todo project

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-let response = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(data => {
-    console.log(data)
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+
+const getData = async (item) => {
+    console.log(item)
+    let url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
+    console.log(url)
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log()
+    data.hits.forEach(recipe => {
+        console.log(recipe)
+    })
+}
+
+//  add event listener to the button
+button.addEventListener('click', (e) => {
+    getData(input.value)
 })
