3527ac5ed25bdf7f38dd0fcbb0727165fd4e0f5a
Angel-12314 <59677750+Angel-12314@users.noreply.github.com>
2024-08-31 12:11:36 +0530

    Update app.js
    Todo List(add & delete)

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
 var body = document.querySelector('body')
-var button = document.querySelector('button')
+//var button = document.querySelector('button') 
+var input = document.querySelector('input')
+var list = document.querySelector('ul')
+var Add = document.querySelector('#Add') 
+var Mode = document.querySelector('#Mode') 
+var chores = []
 
 
-const callbackfunc = (event) => {
+const mode = (event) => {
     body.classList.toggle('dark')
 }
 
+var deleteItem = (value) =>{
+    const index = chores.indexOf(value)
+    console.log(index)
+    chores.splice(index,1)
+    console.log(chores)
+    
+}
 
-button.addEventListener('click', callbackfunc)
+const callbackfunc = (event) =>{
+    const inputValue = input.value
+    //console.log(inputValue)
+        if (chores.includes(inputValue)) {
+            console.log('already exists')
+        }
+        else {
+            chores.push(inputValue)
+            console.log(event.target)
+            console.log(input.value)
+            const element = document.createElement('li')
+            const textNode = document.createTextNode(inputValue)
+            element.appendChild(textNode)
+            list.appendChild(element)
+            element.addEventListener('click', (e) => {
+                deleteItem(e.target.remove())
+            })
+        }
+    
+}
+
+Add.addEventListener('click',callbackfunc)
+Mode.addEventListener('click',mode)
+
