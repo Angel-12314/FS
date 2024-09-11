01234ea0fe1b65df02a35295028836fd5d0350ae
Angel-12314 <59677750+Angel-12314@users.noreply.github.com>
2024-09-11 12:18:25 +0530

    headers & authentication

--- server.js
+++ server.js
@@ -1,33 +1,16 @@
-/*const http = require('http')
-const fs = require('fs')
-const path = require('path')
-
-console.log(http)
-
-const server = http.createServer((request,response) => {
-    const {url} = request
-    if (url =='/login'){
-        response.write('<h1>LOGIN</h1>')
-        response.end()
-    }
-    if(url =='/signup'){
-        response.write('<h1>SIGNUP</h1>')
-        response.end()
-    } 
-})
-
-server.listen(3000,() => {
-    console.log(`Server listening at PORT : ${3000}`)
-})*/
-
-const data = [{name:"A"}, {name:"B"}, {name:"C"}]
 const express = require('express')
-const app = express()
+const PORT = 3000
+const app= express()
 
-app.get('/getNames',(req,res)=>{
-    console.log("Server listening at port 3000")
+const verify = (req,res,next)=>{
+    if(req.headers['user-agent'] === "Thunder Client (https://www.thunderclient.com)") next()
+    else res.send("BLOCKED")
+}
+
+app.get('/',verify, (req,res)=>{
+    res.send("VERIFIED")
 })
 
-app.listen(3000,()=>{
-    console.log("Server listening at port 3000")
+app.listen(PORT,()=>{
+    console.log(`Server listening at port ${PORT}`)
 })
